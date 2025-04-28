0b3b4955d1916fe900ad69265a5143264fd0d8bf
CarlJones1971 <carl.jones@southwales.ac.uk>
2025-04-28 21:02:35 +0100

    Delete src/api/getToken directory

--- src/api/getToken/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function (context, req) {
-    context.res = {
-        body: { 
-            status: "Jonny 5 is alive!",
-            timestamp: new Date().toISOString() 
-        }
-    };
-};
